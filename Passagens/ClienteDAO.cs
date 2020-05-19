44d27ce091d6a4ab3310a77e0c4ffafbe0e24361
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-05-19 12:50:12 -0300

    Criando o primeiro serviço

---
+++ Passagens/ClienteDAO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Passagens
+{
+    class ClienteDAO
+    {
+        private static IList<Cliente> clientes = new List<Cliente>();
+
+        public void Add(Cliente c)
+        {
+            ClienteDAO.clientes.Add(c);
+        }
+
+        public Cliente Buscar(string nome)
+        {
+            var resultado = ClienteDAO.clientes.Where(c => c.Nome.Equals(nome));
+            return (Cliente)resultado;
+        }
+    }
+}
---
+++ Passagens/ClienteService.cs
@@ -0,0 +1,21 @@
+﻿namespace Passagens
+{
+    class ClienteService : IclienteService
+    {
+        private readonly ClienteDAO dao;
+
+        public ClienteService(ClienteDAO clienteDAO)
+        {
+            this.dao = clienteDAO;
+        }
+        public void Add(Cliente c)
+        {
+            dao.Add(c);
+        }
+
+        public Cliente Buscar(string nome)
+        {
+            return dao.Buscar(nome);
+        }
+    }
+}
